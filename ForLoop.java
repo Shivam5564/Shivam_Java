41458a193de64866cc4665d1ce6630d4a2b3fd98
Shivam <shivam@shivam.bhatnagar>
2024-04-02 19:18:31 +0530

    forloop added

---
+++ ForLoop.java
@@ -0,0 +1,22 @@
+package MyPackage;
+
+public class ForLoop {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+//		for (int i=1; i<100; i++) {
+//			
+//			System.out.println("India");
+//			Thread.sleep(1000);
+		
+		for (int i=1; i<=10; i++) {
+			
+			
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
